8a57556eb3a48642f2ab177e2af61031ad6bc353
brandontzhang <zhangtbrandon2@gmail.com>
2023-09-27 21:07:21 -0400

    Clean up import

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,4 @@
 import { AddStoreModal } from "@/components/modals/add-store-modal";
-import Navbar from "@/components/nav/navbar";
 import { auth } from "@clerk/nextjs";
 import axios from "axios";
 import { redirect } from "next/navigation";
