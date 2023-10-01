fd599c79ed040bc902888f94f2d4b1c23be5c526
brandontzhang <zhangtbrandon2@gmail.com>
2023-10-01 14:51:32 -0400

    Add route for getting specific store with id

--- app/api/stores/[storeid]/route.ts
+++ app/api/stores/[storeid]/route.ts
@@ -1,7 +1,25 @@
 import prismadb from "@/lib/prismadb";
 import { NextResponse } from "next/server";
 
-export async function DELETE(request: Request, { params } : { params : {storeid : string}}) {
+export async function GET(
+    _request: Request,
+    { params }: { params: { storeid: string } },
+) {
+    const { storeid } = params;
+
+    const res = await prismadb.store.findUnique({
+        where: {
+            id: storeid,
+        },
+    });
+
+    return NextResponse.json(res);
+}
+
+export async function DELETE(
+    _request: Request,
+    { params }: { params: { storeid: string } },
+) {
     const { storeid } = params;
 
     const res = await prismadb.store.delete({
